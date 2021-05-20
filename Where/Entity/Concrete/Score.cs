4277d6b7dcb8e0000fae5998eaaaa580531a35c2
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-20 22:33:38 +0300

    Create Score.cs

---
+++ Where/Entity/Concrete/Score.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entity.Concrete
+{
+    public class Score
+    {
+        public double Id { get; set; }
+        public double ServiceScore { get; set; }
+        public double CovidScore { get; set; }
+    }
+}
