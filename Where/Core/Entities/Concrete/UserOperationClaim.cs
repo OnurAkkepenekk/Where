23d81eae11ebd77ee987f62705899daf0b4eac54
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-20 22:23:33 +0300

    Update UserOperationClaim.cs

--- Where/Core/Entities/Concrete/UserOperationClaim.cs
+++ Where/Core/Entities/Concrete/UserOperationClaim.cs
@@ -6,5 +6,8 @@ namespace Core.Entities.Concrete
 {
     class UserOperationClaim
     {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int OperationClaimId { get; set; }
     }
 }
