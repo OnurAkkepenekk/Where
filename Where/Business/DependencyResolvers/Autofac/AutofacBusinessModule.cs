22e3bef1535b4042c6fd679f0d8cb70b7a9d1cea
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-21 16:28:52 +0300

    Create AutofacBusinessModule.cs

---
+++ Where/Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Security.JWT;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
+            builder.RegisterType<AuthManager>().As<IAuthService>();
+            builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            //builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+            //    .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+            //    {
+            //        Selector = new AspectInterceptorSelector()
+            //    }).SingleInstance();
+
+        }
+    }
+}
