481d8205d278284c71e0d517789517dc380b9920
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-21 16:28:42 +0300

    Create EfUserDal.cs

---
+++ Where/DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,24 @@
+﻿using Core.DataAccess.EntityFramework;
+using Core.Entities.Concrete;
+using DataAccess.Abstract;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal : EfEntityRepositoryBase<User, WhereDatabaseContext>, IUserDal
+    {
+        public List<OperationClaim> GetClaims(User user)
+        {
+            using (var context = new WhereDatabaseContext())
+            {
+                var result = from operationClaim in context.OperationClaims
+                             join userOperationClaim in context.UserOperationClaims
+                                 on operationClaim.Id equals userOperationClaim.OperationClaimId
+                             where userOperationClaim.UserId == user.Id
+                             select new OperationClaim { Id = operationClaim.Id, Name = operationClaim.Name };
+                return result.ToList();
+            }
+        }
+    }
+}
