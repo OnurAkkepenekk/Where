487a18a3fb0a7b469df64d2d32adcefedb4d6643
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-05-21 18:06:19 +0300

    add comment manager

---
+++ Where/Business/Concrete/CommentManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CommentManager : ICommentService
+    {
+        ICommentDal _commentDal;
+        public CommentManager(ICommentDal commentDal)
+        {
+            _commentDal = commentDal;
+        }
+        public IResult Add(Comment comment)
+        {
+            _commentDal.Add(comment);
+            return new SuccessResult(Messages.AddComment);
+        }
+
+        public IResult Delete(Comment comment)
+        {
+            _commentDal.Delete(comment);
+            return new SuccessResult(Messages.DeleteComment);
+        }
+
+        public IDataResult<List<Comment>> GetByLocationId(int placeId)
+        {
+            return new SuccessDataResult<List<Comment>>(_commentDal.GetAll(c => c.PlaceId == placeId), Messages.CommentGetById);
+        }
+
+        public IResult Update(Comment comment)
+        {
+            _commentDal.Update(comment);
+            return new SuccessResult(Messages.UpdateComment);
+        }
+    }
+}
