5d6389f5b99cb098798ac2ca75b0391cf7b72db3
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-05-21 18:06:45 +0300

    add score manager

---
+++ Where/Business/Concrete/ScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ScoreManager : IScoreService
+    {
+        IScoreDal _scoreDal;
+        public ScoreManager(IScoreDal scoreDal)
+        {
+            _scoreDal = scoreDal;
+        }
+        public IResult Add(Score score)
+        {
+            _scoreDal.Add(score);
+            return new SuccessResult(Messages.AddScore);
+        }
+
+        public IResult Delete(Score score)
+        {
+            _scoreDal.Delete(score);
+            return new SuccessResult(Messages.DeleteScore);
+        }
+
+        public IDataResult<List<Score>> GetByLocationId(int placeId)
+        {
+            return new SuccessDataResult<List<Score>>(_scoreDal.GetAll(s => s.PlaceId == placeId), Messages.ScoreGetById);
+        }
+
+        public IResult Update(Score score)
+        {
+            _scoreDal.Update(score);
+            return new SuccessResult(Messages.UpdateScore);
+        }
+    }
+}
