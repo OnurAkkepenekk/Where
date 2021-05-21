3b46ec7f01b69d05b3016acb06cc18f66e25b546
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-21 16:28:50 +0300

    Create Messages.cs

---
+++ Where/Business/Constants/Messages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string UserAdded = "Kullanıcı Eklendi.";
+        public static string UserDeleted = "Kullanıcı Silindi.";
+        public static string UserUpdated = "Kullanıcı Güncellendi.";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserRegistered = "Kullanıcı kayıt işlemi tamamlandı.";
+        public static string PasswordError = "Şifre yanlış";
+        public static string SuccessfulLogin = "Giriş başarılı";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+        public static string AccessTokenCreated = "Token oluşturuldu";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+    }
+}
