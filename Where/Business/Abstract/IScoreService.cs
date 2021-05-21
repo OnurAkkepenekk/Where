37dae124daf030e64c6fdfae67afdf9ab8858646
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-05-21 18:06:08 +0300

    add score service

---
+++ Where/Business/Abstract/IScoreService.cs
@@ -0,0 +1,16 @@
+﻿using Core.Utilities.Results;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IScoreService
+    {
+        IResult Add(Score score);
+        IResult Delete(Score score);
+        IResult Update(Score score);
+        IDataResult<List<Score>> GetByLocationId(int locationId);
+    }
+}
