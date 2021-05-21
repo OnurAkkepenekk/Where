3cf8766bac091856f7fe4af7495439895aa75811
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-21 16:28:38 +0300

    Create WhereDatabaseContext.cs

---
+++ Where/DataAccess/Concrete/EntityFramework/WhereDatabaseContext.cs
@@ -0,0 +1,22 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class WhereDatabaseContext : DbContext
+    {
+        // Hangi veritabanı
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB; Database=Where; Trusted_Connection=true");
+
+        }
+        // Hangi tablo nereye karşılık gelicek
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+    }
+}
