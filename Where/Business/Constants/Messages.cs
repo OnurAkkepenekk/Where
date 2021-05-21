745f5d30a566cb922548bf5f5eddd389a747bd0d
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-05-21 18:05:22 +0300

    update

--- Where/Business/Constants/Messages.cs
+++ Where/Business/Constants/Messages.cs
@@ -9,12 +9,20 @@ namespace Business.Constants
         public static string UserAdded = "Kullanıcı Eklendi.";
         public static string UserDeleted = "Kullanıcı Silindi.";
         public static string UserUpdated = "Kullanıcı Güncellendi.";
-        public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
         public static string UserRegistered = "Kullanıcı kayıt işlemi tamamlandı.";
-        public static string PasswordError = "Şifre yanlış";
-        public static string SuccessfulLogin = "Giriş başarılı";
-        public static string UserAlreadyExists = "Kullanıcı mevcut";
-        public static string AccessTokenCreated = "Token oluşturuldu";
-        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Şifre yanlış.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string UserAlreadyExists = "Kullanıcı mevcut.";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string AddComment = "Yorum eklendi.";
+        public static string UpdateComment = "Yorum güncellendi.";
+        public static string DeleteComment = "Yorum silindi.";
+        public static string CommentGetById = "Mekana göre yorumlar listelendi.";
+        public static string AddScore = "Degerlendirme eklendi.";
+        public static string UpdateScore = "Degerlendirme güncellendi.";
+        public static string DeleteScore = "Degerlendirme silindi.";
+        public static string ScoreGetById = "Mekana göre degerlendirmeler listelendi.";
     }
 }
