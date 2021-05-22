1baf52e618e79a2956063a416c5e691687bc7e04
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-22 17:48:57 +0300

    Update Score.cs

--- Where/Entity/Concrete/Score.cs
+++ Where/Entity/Concrete/Score.cs
@@ -7,8 +7,8 @@ namespace Entity.Concrete
 {
     public class Score : IEntity
     {
-        public double Id { get; set; }
-        public double PlaceId { get; set; }
+        public int Id { get; set; }
+        public int PlaceId { get; set; }
         public double ServiceScore { get; set; }
         public double CovidScore { get; set; }
     }
