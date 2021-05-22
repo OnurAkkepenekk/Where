c87b0dbe46f0103c47b94156e4f182c329ceb0ca
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-22 15:39:44 +0300

    Create CommetsController.cs

---
+++ Where/WebAPI/Controllers/CommetsController.cs
@@ -0,0 +1,62 @@
+﻿using Business.Abstract;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommetsController : ControllerBase
+    {
+        ICommentService _commentService;
+
+        public CommetsController(ICommentService _commentService)
+        {
+            this._commentService = _commentService;
+        }
+
+        [HttpGet("GetById")]
+        public IActionResult GetByPlaceId(int id)
+        {
+            var result = _commentService.GetByLocationId(id);
+            if (result.Success)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(Comment comment)
+        {
+            var result = _commentService.Add(comment);
+            if (result.Success)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+
+        [HttpPost("Delete")]
+        public IActionResult Delete(Comment comment)
+        {
+            var result = _commentService.Delete(comment);
+            if (result.Success)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+
+        [HttpPost("Update")]
+        public IActionResult Update(Comment comment)
+        {
+            var result = _commentService.Update(comment);
+            if (result.Success)
+                return Ok(result);
+            else
+                return BadRequest(result);
+        }
+    }
+}
