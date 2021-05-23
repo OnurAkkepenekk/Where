54c51586cba7d81b0cd9eb17f10c6e7d96054721
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-05-23 16:03:36 +0300

    Create ScoreValidator

---
+++ Where/Business/ValidationRules/FluentValidation/ScoreValidator.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ScoreValidator : AbstractValidator<Score>
+    {
+        public ScoreValidator()
+        {
+            RuleFor(s => s.PlaceId).NotEmpty();
+            RuleFor(s => s.ServiceScore).NotEmpty();
+            RuleFor(s => s.CovidScore).NotEmpty();
+            RuleFor(s => s.ServiceScore).GreaterThanOrEqualTo(0);
+            RuleFor(s => s.ServiceScore).LessThanOrEqualTo(5);
+            RuleFor(s => s.CovidScore).GreaterThanOrEqualTo(0);
+            RuleFor(s => s.CovidScore).LessThanOrEqualTo(5);
+        }
+    }
+}
