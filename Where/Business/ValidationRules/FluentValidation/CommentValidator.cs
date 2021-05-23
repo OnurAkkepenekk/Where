a9b7d1100887e23e6836db7c092c2d673f3456c0
Yasemin Gerboğa <yasemingerboga@gmail.com>
2021-05-23 16:03:24 +0300

    Create CommentValidator

---
+++ Where/Business/ValidationRules/FluentValidation/CommentValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CommentValidator : AbstractValidator<Comment>
+    {
+        public CommentValidator()
+        {
+            RuleFor(c => c.PlaceId).NotEmpty();
+            RuleFor(c => c.CommentText).NotEmpty();
+            RuleFor(c => c.CommentText).MaximumLength(160);
+        }
+    }
+}
