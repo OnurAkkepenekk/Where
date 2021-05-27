2b8cb06808451afa57aecab9108b1d7f11dd85d5
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-27 23:35:28 +0300

    Add Middleware

--- Where/WebAPI/Startup.cs
+++ Where/WebAPI/Startup.cs
@@ -1,4 +1,5 @@
 using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
@@ -69,6 +70,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
+            app.ConfigureCustomExceptionMiddleware();
 
             app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
