9430c0cd599ff2d416fed3b31d97f241e51f16e0
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-05-27 18:28:17 +0300

    bugfix location of extensions file.

--- Where/WebAPI/Startup.cs
+++ Where/WebAPI/Startup.cs
@@ -70,7 +70,7 @@ namespace WebAPI
             }
 
 
-            app.UseCors(builder => builder.WithOrigins("http://localhost:8080").AllowAnyHeader());
+            app.UseCors(builder => builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
