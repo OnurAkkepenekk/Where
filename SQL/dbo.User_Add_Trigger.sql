e68807b56865e52d23d26ab470d2ff8e253705f2
Onur Akkepenek <152120171023@ogrenci.ogu.edu.tr>
2021-06-03 20:59:53 +0300

    Create dbo.User_Add_Trigger.sql

---
+++ SQL/dbo.User_Add_Trigger.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER [User_Add_Trigger]
+	ON [dbo].[Users]
+	FOR INSERT
+	AS
+	DECLARE @UserId int, @OperationClaimId INT;
+	IF exists(SELECT * FROM Users) and exists (SELECT * FROM OperationClaims)
+	BEGIN
+		SET @UserId = (SELECT TOP 1 Id FROM Users ORDER BY Id DESC);
+		SET @OperationClaimId = 1;
+		INSERT INTO UserOperationClaims VALUES (@UserId,@OperationClaimId);
+	END
